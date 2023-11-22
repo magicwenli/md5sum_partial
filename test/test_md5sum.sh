c41d708a668150f74df98f0f07b669a7d49356e7
magicwenli <yxnian@outlook.com>
2023-11-22 02:23:56 +0000

    feat: add test case

--- test/test_md5sum.sh
+++ test/test_md5sum.sh
@@ -24,17 +24,31 @@ compare_md5sum() {
 input="$script_dir/test.txt"
 expect=$(md5sum "$input" | awk '{print $1}')
 actual=$(./$my_bin "$input" | awk '{print $1}')
-compare_md5sum "test_txt_full" "$input" "$expect" "$actual"
+compare_md5sum "test_tty_full" "$input" "$expect" "$actual"
 
 # Test case 2: Compare md5sum output with bin file
 input="$script_dir/test.bin"
 expect=$(md5sum "$input" | awk '{print $1}')
 actual=$(./$my_bin "$input" | awk '{print $1}')
-compare_md5sum "test_bin_full" "$input" "$expect" "$actual"
+compare_md5sum "test_binary_full" "$input" "$expect" "$actual"
 
 # Test case 3: Compare md5sum output with partial bin file
 input="$script_dir/test.bin"
 size=500
 expect=$(dd if=$input bs=$size count=1 2> /dev/null | md5sum | awk '{print $1}')
 actual=$(./$my_bin -l $size "$input" | awk '{print $1}')
-compare_md5sum "test_bin_partial" "$input" "$expect" "$actual"
+compare_md5sum "test_binary_partial" "$input" "$expect" "$actual"
+
+# Test case 4: Compare md5sum output with partial bin file again
+input="$script_dir/test.bin"
+size=768
+expect=$(dd if=$input bs=$size count=1 2> /dev/null | md5sum | awk '{print $1}')
+actual=$(./$my_bin -l $size "$input" | awk '{print $1}')
+compare_md5sum "test_binary_partial 2" "$input" "$expect" "$actual"
+
+# Test case 5: Compare md5sum output with /dev/mem
+input="/dev/mem"
+size=768
+expect=$(sudo dd if=$input bs=$size count=1 2> /dev/null | md5sum | awk '{print $1}')
+actual=$(sudo ./$my_bin -l $size "$input" | awk '{print $1}')
+compare_md5sum "test_device_partial" "$input" "$expect" "$actual"
